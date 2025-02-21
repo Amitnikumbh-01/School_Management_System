b56eba2f69b1ae2d8c0db295c30e5c0d121599d1
Amitnikumbh-01 <133536407+Amitnikumbh-01@users.noreply.github.com>
2025-02-21 16:06:27 +0530

    Update assignmentSlice.js

--- frontend/src/redux/slices/assignmentSlice.js
+++ frontend/src/redux/slices/assignmentSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/assignments';
+const API_URL = 'https://school-management-system-back.onrender.com/api/assignments';
 
 // Get assignments
 export const getAssignments = createAsyncThunk(
