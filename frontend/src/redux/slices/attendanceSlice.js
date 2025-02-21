b62e198fa2e105fb231dfb695d76158de2c3e41e
Amitnikumbh-01 <133536407+Amitnikumbh-01@users.noreply.github.com>
2025-02-21 16:06:12 +0530

    Update attendanceSlice.js

--- frontend/src/redux/slices/attendanceSlice.js
+++ frontend/src/redux/slices/attendanceSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/attendance';
+const API_URL = 'https://school-management-system-back.onrender.com/api/attendance';
 
 export const markAttendance = createAsyncThunk(
   'attendance/mark',
